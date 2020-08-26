77aab224469692c1f31caef805cac2405624f235
Максимов Максим <51723172+gvozd0072008@users.noreply.github.com>
2020-08-26 12:26:23 +0300

    bug fix
    bug fix

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Calculator {
     private static boolean arabic_numbers = true;
+    private static boolean strI1 = true;
+    private static boolean strI2 = true;
+
 
     private static String[] pars(String input) {
         String[] parsedInput = input.split(" ");
@@ -19,40 +22,49 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите выражение, разделяя каждый символ пробелом: ");
         String input = scanner.nextLine();
-        while (!input.equals(null)) {
-            String[] values1 = Calculator.pars(input);
-            String operation = values1[1];
-            Number values;
-            int i = 0;
-            int i2 = 0;
-            try {
-                i = Integer.parseInt(values1[0]);
-                i2 = Integer.parseInt(values1[2]);
-            } catch (NumberFormatException e) {
-                arabic_numbers = false;
-            }
-            if (arabic_numbers) {
-                values = new ArabicNumber(i, i2);
-            } else {
-                values = new RomesNumer(values1[0], values1[2]);
-            }
-            if (operation.equals("+")) {
-                values.sum();
-            } else if (operation.equals("-")) {
-                values.sub();
-            } else if (operation.equals("/")) {
-                values.div();
-            } else if (operation.equals("*")) {
-                values.mul();
-            }
+       try {
+           while (!input.equals(null)) {
+               String[] values1 = Calculator.pars(input);
+               String operation = values1[1];
+               Number values;
+               int i = 0;
+               int i2 = 0;
+               try {
+                   i = Integer.parseInt(values1[0]);
+               } catch (NumberFormatException e) {
+                   strI1 = false;
+               }
+               try {
+                   i2 = Integer.parseInt(values1[2]);
+               } catch (NumberFormatException e) {
+                   strI2 = false;
+               }
+               if (strI1 && strI2) {
+                   arabic_numbers = false;
+               }
+               if (arabic_numbers) {
+                   values = new ArabicNumber(i, i2);
+               } else {
+                   values = new RomesNumer(values1[0], values1[2]);
+               }
+               if (operation.equals("+")) {
+                   values.sum();
+               } else if (operation.equals("-")) {
+                   values.sub();
+               } else if (operation.equals("/")) {
+                   values.div();
+               } else if (operation.equals("*")) {
+                   values.mul();
+               }
 
-            if (arabic_numbers) {
-                System.out.println("Ответ: " + values.getResult());
-            } else {
-                System.out.println("Ответ: " + values.getStringResult());
-            }
-            System.out.println();
-            input = null;
-        }
+               if (arabic_numbers) {
+                   System.out.println("Ответ: " + values.getResult());
+               } else {
+                   System.out.println("Ответ: " + values.getStringResult());
+               }
+               System.out.println();
+               input = null;
+           }
+       } catch (NullPointerException e){}
     }
 }
